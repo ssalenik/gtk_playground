b6ea171c5d9ba6219fe51a4e0a3dfb036788702c
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2014-08-01 14:57:23 -0400

    made test button selection

--- src/test_gui.c
+++ src/test_gui.c
@@ -1,13 +1,42 @@
 #include <gtk/gtk.h>
 
+gboolean draw_button(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+	if(gtk_widget_has_focus(widget)) {
+		double r = 0;
+		double g = 102.0/255.0;
+		double b = 1.0;
+		cairo_rectangle (
+			cr,
+			0, 0,
+			gtk_widget_get_allocated_width(widget),
+			gtk_widget_get_allocated_height(widget));
+		cairo_set_source_rgba (cr, r, g, b, 0.25);
+		cairo_fill (cr);
+
+		// cairo_set_line_width (cr, 2);
+		// cairo_set_source_rgba (cr, r, g, b, 0.5);
+		// cairo_rectangle (cr, 0, 0, 
+		// 	gtk_widget_get_allocated_width(widget),
+		// 	gtk_widget_get_allocated_height(widget));
+		// cairo_stroke (cr);
+	} else {
+		// do nothing
+	}
+			
+	return FALSE;
+}
+
 int
 main( int    argc,
 	  char **argv )
 {
 	/* Vars */
 	GtkWidget  *window;
+	GtkWidget  *button1, *button2, *button3;
 	GtkBuilder *builder;
-	GError     *error;
+	GError     *error = NULL;
+	// gboolean	*selected = (gboolean*)g_malloc(sizeof(gboolean));
+	// *selected = FALSE;
 
 	/* Initialization */
 	gtk_init( &argc, &argv );
@@ -25,7 +54,29 @@ main( int    argc,
 
 	/* Get main window and connect signals */
 	window = GTK_WIDGET( gtk_builder_get_object( builder, "window_main" ) );
+	button1 = GTK_WIDGET( gtk_builder_get_object( builder, "button1" ) );
+	button2 = GTK_WIDGET( gtk_builder_get_object( builder, "button2" ) );
+	button3 = GTK_WIDGET( gtk_builder_get_object( builder, "button3" ) );
 	gtk_builder_connect_signals( builder, NULL );
+
+	g_signal_connect_after (
+		button1,
+		"draw",
+		G_CALLBACK (draw_button),
+		NULL
+	);
+	g_signal_connect_after (
+		button2,
+		"draw",
+		G_CALLBACK (draw_button),
+		NULL
+	);
+	g_signal_connect_after (
+		button3,
+		"draw",
+		G_CALLBACK (draw_button),
+		NULL
+	);
 
 	/* Destroy builder */
 	g_object_unref( G_OBJECT( builder ) );
@@ -36,3 +87,4 @@ main( int    argc,
 
 	return( 0 );
 }
+
